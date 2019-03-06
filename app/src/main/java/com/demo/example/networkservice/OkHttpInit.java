b01b64c002819fe978e3cdba5edbe7fe7d8386f0
Chetan Ansel <chetanansel24@gmail.com>
2019-03-06 11:43:18 +0000

    Update OkHttpInit.java

--- app/src/main/java/com/demo/example/networkservice/OkHttpInit.java
+++ app/src/main/java/com/demo/example/networkservice/OkHttpInit.java
@@ -14,29 +14,29 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class OkHttpInit {
 
+    private static final String KEY = "$2a$10$4juhafP3gPiRY/GznlAfYeex4qnXVqD4/zGl6IXLaP70PxdUfI3ue";
+    private static final String SECRET_KEY = "secret-key";
+
     public Retrofit getHttpClient() {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .connectTimeout(3, TimeUnit.MINUTES)
                 .writeTimeout(3, TimeUnit.MINUTES)
                 .readTimeout(3, TimeUnit.MINUTES)
                 .addInterceptor(new Interceptor() {
-            @Override
-            public Response intercept(@NonNull Chain chain) throws IOException {
-                Request request = chain.request()
-                        .newBuilder()
-                        .addHeader("secret-key", "$2a$10$4juhafP3gPiRY/GznlAfYeex4qnXVqD4/zGl6IXLaP70PxdUfI3ue")
-
-                        .build();
-                return chain.proceed(request);
-            }
-        }).build();
-        Retrofit retrofit = new Retrofit.Builder()
+                    @Override
+                    public Response intercept(@NonNull Chain chain) throws IOException {
+                        Request request = chain.request()
+                                .newBuilder()
+                                .addHeader(SECRET_KEY, KEY)
+                                .build();
+                        return chain.proceed(request);
+                    }
+                }).build();
+        return new Retrofit.Builder()
                 .baseUrl(NetworkInterface.API_HOST_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
-
-        return retrofit;
     }
 
 }
