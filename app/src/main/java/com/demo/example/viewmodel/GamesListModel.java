56b67dfea060e7c89380c504ce8368a717d84108
Chetan Ansel <chetanansel24@gmail.com>
2019-03-06 11:45:03 +0000

    Update GamesListModel.java

--- app/src/main/java/com/demo/example/viewmodel/GamesListModel.java
+++ app/src/main/java/com/demo/example/viewmodel/GamesListModel.java
@@ -1,8 +1,7 @@
 package com.demo.example.viewmodel;
 
-import android.app.Application;
-import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
 import android.support.annotation.Nullable;
 
 import com.demo.example.networkservice.GetGamesAPI;
@@ -10,17 +9,10 @@ import com.demo.example.networkservice.model.GamesData;
 
 import java.util.List;
 
-public class GamesListModel extends AndroidViewModel {
-
-    private final LiveData<List<GamesData>> gamesListObservable;
-
-    public GamesListModel(Application application) {
-        super(application);
-        gamesListObservable = GetGamesAPI.getInstance().getGamesList();
-    }
+public class GamesListModel extends ViewModel {
 
     @Nullable
     public LiveData<List<GamesData>> getGamesListObservable() {
-        return gamesListObservable;
+        return GetGamesAPI.getInstance().getGamesList();
     }
 }
